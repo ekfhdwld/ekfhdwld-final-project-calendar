837e171bae93aced76d1866812c720480a5dcab1
jihye <mickey0426@naver.com>
2022-01-25 23:51:55 +0900

    Add domain-core
    도메인 pojo 객체 생성

---
+++ calendar-core/src/main/java/com/jihye/fc/finalproject/core/domain/User.java
@@ -0,0 +1,29 @@
+package com.jihye.fc.finalproject.core.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@Getter
+public class User {
+	
+	private Long id;
+	private String name;
+	private String email;
+	private String password;
+	private LocalDate birthday;
+	private LocalDateTime createAt;
+	
+	public User(String name, String email, String password, LocalDate birthday, LocalDateTime createAt) {
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		this.birthday = birthday;
+		this.createAt = createAt;
+	}
+	
+	
+}
