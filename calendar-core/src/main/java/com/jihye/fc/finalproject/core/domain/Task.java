ad22befa87ec448013c943a6e943f48a190f9175
jihye <mickey0426@naver.com>
2022-01-26 22:23:28 +0900

    Set Entity Relation
    Schedule 테이블 하나에 3개의 도메인 연결

--- calendar-core/src/main/java/com/jihye/fc/finalproject/core/domain/Task.java
+++ calendar-core/src/main/java/com/jihye/fc/finalproject/core/domain/Task.java
@@ -1,19 +1,20 @@
 package com.jihye.fc.finalproject.core.domain;
 
+import com.jihye.fc.finalproject.core.domain.entity.Schedule;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
 
 @NoArgsConstructor
 @Getter
 public class Task {
 	
-	private Long id;
-	private LocalDateTime taskAt;
-	private String title;
-	private String description;
-	private User writer;
-	private LocalDateTime createdAt;
+	private Schedule schedule;
 	
+	public Task(Schedule schedule) {
+		this.schedule = schedule;
+	}
+	
+	public String getTitle() {
+		return schedule.getTitle();
+	}
 }
