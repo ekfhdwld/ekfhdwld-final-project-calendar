282ffdeec11d7a5195c60090ae8bbd508064b589
jihye <mickey0426@naver.com>
2022-01-27 16:45:09 +0900

    Extract BaseEntity
    - User, Engagement, Schedule 간 공통 속성을 BaseEntity로 추출
    - jpa auditing 기능을 사용해 createdAt, updatedAt 컬럼 추가
    - auditing 등록

--- calendar-core/src/main/java/com/jihye/fc/finalproject/core/domain/entity/User.java
+++ calendar-core/src/main/java/com/jihye/fc/finalproject/core/domain/entity/User.java
@@ -3,25 +3,21 @@ package com.jihye.fc.finalproject.core.domain.entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 
 @NoArgsConstructor
 @Getter
 @Table(name = "users")
 @Entity
-public class User {
+public class User extends BaseEntity{
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
 	private String name;
 	private String email;
 	private String password;
 	private LocalDate birthday;
-	private LocalDateTime createAt = LocalDateTime.now();
 	
 	public User(String name, String email, String password, LocalDate birthday) {
 		this.name = name;
