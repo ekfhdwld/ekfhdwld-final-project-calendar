8e01b905d1335ffb2c87bb4fa749f1134165faf4
jihye <mickey0426@naver.com>
2022-01-28 22:30:25 +0900

    Add Repository

---
+++ calendar-core/src/main/java/com/jihye/fc/finalproject/core/domain/entity/repository/EngagementRepository.java
@@ -0,0 +1,7 @@
+package com.jihye.fc.finalproject.core.domain.entity.repository;
+
+import com.jihye.fc.finalproject.core.domain.entity.Engagement;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EngagementRepository extends JpaRepository<Engagement, Long> {
+}
