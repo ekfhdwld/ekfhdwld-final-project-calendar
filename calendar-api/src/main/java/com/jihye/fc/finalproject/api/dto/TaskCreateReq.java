ca65911f538f03e1f89f983b5bcd0f23d7a1c002
jihye <mickey0426@naver.com>
2022-01-29 22:20:45 +0900

    Add TaskService, AuthUser, TaskCreateReq
    - task 생성 로직 개발
    - HttpSession을 서비스로 넣지 않고, AuthUser 도출
    - userService에 findByUserId 메소드 추가

---
+++ calendar-api/src/main/java/com/jihye/fc/finalproject/api/dto/TaskCreateReq.java
@@ -0,0 +1,12 @@
+package com.jihye.fc.finalproject.api.dto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class TaskCreateReq {
+	private String title;
+	private String description;
+	private LocalDateTime taskAt;
+}
