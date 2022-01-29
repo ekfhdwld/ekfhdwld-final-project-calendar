14189ab78ce484ed78486eee8e1d67b0cf1a285c
jihye <mickey0426@naver.com>
2022-01-29 16:57:39 +0900

    Refactor ApiApplication
    - CoreConfig와 중첩되는 애노테이션 삭제
    - save() 메소드 내용 정리

--- calendar-api/src/main/java/com/jihye/fc/finalproject/api/ApiApplication.java
+++ calendar-api/src/main/java/com/jihye/fc/finalproject/api/ApiApplication.java
@@ -4,17 +4,11 @@ import com.jihye.fc.finalproject.core.SimpleEntity;
 import com.jihye.fc.finalproject.core.SimpleEntityRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@EnableJpaAuditing
-@EntityScan("com.jihye.fc.finalproject.core")
-@EnableJpaRepositories("com.jihye.fc.finalproject.core")
 @RestController
 @SpringBootApplication(scanBasePackages = "com.jihye.fc.finalproject")
 public class ApiApplication {
@@ -32,11 +26,8 @@ public class ApiApplication {
 
     @GetMapping("/save")
     public SimpleEntity saveOne(){
-        final SimpleEntity e = new SimpleEntity();
-        e.setName("hello");
-        return repository.save(e);
+        return repository.save(new SimpleEntity());
     }
-
 
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
