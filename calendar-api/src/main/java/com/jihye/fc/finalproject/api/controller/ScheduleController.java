8b85d1c2f6293c23e3bba398906bb04dd22a0c27
jihye <mickey0426@naver.com>
2022-03-16 14:24:28 +0900

    add ArgumentResolver, WebConfig
    - argument resolver와 이를 리스트에 추가하는 설정 파일 추가

--- calendar-api/src/main/java/com/jihye/fc/finalproject/api/controller/ScheduleController.java
+++ calendar-api/src/main/java/com/jihye/fc/finalproject/api/controller/ScheduleController.java
@@ -1,7 +1,9 @@
 package com.jihye.fc.finalproject.api.controller;
 
 import com.jihye.fc.finalproject.api.dto.AuthUser;
+import com.jihye.fc.finalproject.api.dto.EventCreateReq;
 import com.jihye.fc.finalproject.api.dto.TaskCreateReq;
+import com.jihye.fc.finalproject.api.service.EventService;
 import com.jihye.fc.finalproject.api.service.TaskService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -10,25 +12,27 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpSession;
-
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/schedules")
 public class ScheduleController {
 	
-	private final static String LOGIN_SESSION_KEY = "USER_ID";
 	private final TaskService taskService;
+	private final EventService eventService;
 	
 	@PostMapping("/tasks")
 	public ResponseEntity<Void> createTask(
 	        @RequestBody TaskCreateReq taskCreateReq,
-	        HttpSession session){
-		final Long userId = (Long) session.getAttribute("LOGIN_SESSION_KEY");
-		if(userId == null){
-			throw new RuntimeException("bad request. no session");
-		}
-		taskService.create(taskCreateReq, AuthUser.of(userId));
+	        AuthUser authUser){
+		taskService.create(taskCreateReq, authUser);
+		return ResponseEntity.ok().build();
+	}
+	
+	@PostMapping("/events")
+	public ResponseEntity<Void> createEvent(
+			@RequestBody EventCreateReq eventCreateReq,
+			AuthUser authUser){
+		eventService.create(eventCreateReq, authUser);
 		return ResponseEntity.ok().build();
 	}
 	
