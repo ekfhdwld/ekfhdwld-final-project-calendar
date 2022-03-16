e25aac20d60b85b159c4db574eb873537756c758
jihye <mickey0426@naver.com>
2022-03-16 14:40:00 +0900

    add EventService
    - event 생성로직 추가
    1. controller
    2. service 생성
    3. engagement 쿼리 메서드 추가
    4. engagement 시간 겹치는 로직 구현 및 테스트 (stackoverflow 참조)
    5. fake email 추가
    6. 생성 테스트

---
+++ calendar-api/src/main/java/com/jihye/fc/finalproject/api/dto/EventCreateReq.java
@@ -0,0 +1,16 @@
+package com.jihye.fc.finalproject.api.dto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+public class EventCreateReq {
+	private String title;
+	private String description;
+	private LocalDateTime startAt;
+	private LocalDateTime endAt;
+	private List<Long> attendeeIds;
+	
+}
